d09209cd992c8690e9756a145e669b9de2bbb355
Sy <simon.tsegay@gmail.com>
2019-03-17 18:08:11 -0400

    Updates

---
+++ precache-manifest.9288e5cf33d6be70851b8a972b760752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dca64b1695a34a06f0e1",
+    "url": "/static/css/main.17917495.chunk.css"
+  },
+  {
+    "revision": "dca64b1695a34a06f0e1",
+    "url": "/static/js/main.2282f973.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cebd844cc7868a9857b7",
+    "url": "/static/js/2.a74880ad.chunk.js"
+  },
+  {
+    "revision": "303c2d7c775f85cdcef7f0a7f80c5b40",
+    "url": "/index.html"
+  }
+];
