52d96ed8a7f8853c93c58cb93224f1f6fcba16c4
Sy <simon.tsegay@gmail.com>
2019-03-24 02:40:33 -0400

    rebooted app

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-const mountNode = document.getElementById("root");
-
-ReactDOM.render(<App />, mountNode);
