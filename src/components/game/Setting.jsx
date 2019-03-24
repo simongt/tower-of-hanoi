8d8a1d836ebb0518780ff746e90131b9c31d3475
Sy <simon.tsegay@gmail.com>
2019-03-24 07:20:44 -0400

    updated game components

--- src/components/game/Setting.jsx
+++ src/components/game/Setting.jsx
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
+import Tower from "./Tower";
+import Layout from "../constants/Layout";
 
 class Setting extends Component {
   render() {
+    const {
+      towers
+    } = this.props;
+    let i = 0;
     return (
       <div style={settingStyle}>
-        <h2 style={{ fontSize: "10vmin" }}>{"Game goes here."}</h2>
+        <div style={{ borderBottom: "1px solid black" }} />
+        <Tower key={towers[i].id} disks={towers[i++].disks} />
+        <div style={{ borderBottom: "1px solid black" }} />
+        <Tower key={towers[i].id} disks={towers[i++].disks} />
+        <div style={{ borderBottom: "1px solid black" }} />
+        <Tower key={towers[i].id} disks={towers[i++].disks} />
+        <div style={{ borderBottom: "1px solid black" }} />
       </div>
     );
   }
 }
 
 const settingStyle = {
+  height: `calc(${Layout.SETTING_HEIGHT})`,
   display: "grid",
-  justifyContent: "center",
-  alignContent: "center",
-  justifyItems: "center",
-  alignItems: "center",
-  color: "white"
+  gridTemplateColumns: `repeat(3, auto ${Layout.TOWER_WIDTH}) auto`,
+  justifyContent: "stretch",
+  alignItems: "end",
 };
 
 export default Setting;
