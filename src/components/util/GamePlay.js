ee1564257357404f6c2fbd26955907f47e4dd68a
Sy <simon.tsegay@gmail.com>
2019-03-25 03:08:33 -0400

    updated util components

--- src/components/util/GamePlay.js
+++ src/components/util/GamePlay.js
@@ -1,12 +1,12 @@
 // Consider defining these methods in App to have more direct access to state
 
 export const isValidDiskMove = (tower={}) => {
-  console.log("(Detecting valid DISK move to TOWER)");
+  console.log("Assuming DISK move to TOWER is valid.");
   return true; // assume true until it's time to implement gameplay logic
 }
 
 export const moveDisk = (tower={}) => {
   // TO-DO: update which tower the disk belongs to
-  console.log("=> DISK moved to TOWER");
+  console.log("DISK has moved to TOWER.");
   return;
 }
